397c07962b830483fddceb43cce3aaf7a117778d
subramanyam-subbu <118761642+subramanyam-subbu@users.noreply.github.com>
2022-12-09 11:26:08 +0530

    1 file modified, 2 added

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 import React from 'react';
-import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { createRoot } from 'react-dom/client';
+//import './index.css';
+import App from './App';
+//import reportWebVitals from './reportWebVitals';
 import ClassComponentLifeCycle from './ClassComponentLifeCycle';
 import SimpleFunctionalComponent from './SimpleFunctionalComponent';
-import read from './CRUD/read';
-import App from './App';
+//import ComponentWithStyles from './ComponentWithStyles';
+//import Home from './ConditionalRendering/Home';
+//import UsingMapFunction from './UsingMapFunction';
+//import { BrowserRouter } from 'react-router-dom';
+//import RoutingMain from './Routing/RoutingMain';
+//import Menu from './Routing/Menu';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <StrictMode>
+  <React.StrictMode>
+    
+    {/* <Home role="admin"></Home> */}
+
+    {/* { <SimpleFunctionalComponent name="Benz" model="111"></SimpleFunctionalComponent> } */}
+
+    {/* {<ClassComponentLifeCycle ></ClassComponentLifeCycle> } */}
+
+    {/*<ClassComponentLifeCycle name="Arun" model="Samsung A7" ></ClassComponentLifeCycle> */}
+    
     {/* <App /> */}
 
-    {/* {<ClassComponentLifeCycle />} */}
+    {/* <UsingMapFunction></UsingMapFunction> */}
 
-    {/* <SimpleFunctionalComponent name="Benz" model="111" /> */}
-
-    <read/>
-
-
-
-
-  </StrictMode>
+    {/* <ComponentWithStyles></ComponentWithStyles> */}
+    
+    {/* {<BrowserRouter>}
+    
+      <Menu></Menu>
+      <RoutingMain></RoutingMain>
+      
+    </BrowserRouter> */}
+  
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
